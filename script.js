47dc1847506b464a0e48bcdd1e45f24a140903d6
Astrid!3 <146867076+astrid5825@users.noreply.github.com>
2024-06-05 00:29:03 +0000

    Tue, Jun 4, 2024, 8:29 PM -04:00

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
+let ownerName = "Astrid Zeyer"; // update the part BETWEEN the "quotes"
 let userName = "YourUsername"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
